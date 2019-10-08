8bdc9eecffbe2f17b0f2b92038333b89c3da8ef5
Frank <frankzou2000@hotmail.com>
2019-10-08 20:33:13 +0000

    added character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $('#tweettextarea').on('input', function() {
+    const tweetValLength = $(this).val().length;
+    const buttonVal = 140; 
+    $(this).siblings().children('.counter').text(buttonVal - tweetValLength);
+    if (140 - tweetValLength < 0) {
+      $(this).siblings().children('.counter').css("color", "red");
+    } else {
+      $(this).siblings().children('.counter').css("color", "black");
+    }
+  });  
+});
