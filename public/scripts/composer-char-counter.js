d332c16ebe4977644847b20fcb82fa837f122c56
Frank <frankzou2000@hotmail.com>
2019-10-08 21:58:50 +0000

    implemented tweeter underneath input

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   $('#tweettextarea').on('input', function() {
     const tweetValLength = $(this).val().length;
     const buttonVal = 140; 
-    $(this).siblings().children('.counter').text(buttonVal - tweetValLength);
+    $(this).siblings("#footertweet").children('.counter').text(buttonVal - tweetValLength);
     if (140 - tweetValLength < 0) {
       $(this).siblings().children('.counter').css("color", "red");
     } else {
