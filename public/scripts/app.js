b0f5ebf8272b667584a1032d50a0bd8878be9d93
Frank <frankzou2000@hotmail.com>
2019-10-09 20:43:46 +0000

    added fetching tweets

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,30 +3,8 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+
+$(document).ready(function() {
 
 //Calculate time between tweet and current time
 const calculateTime = function(milliseconds) {
@@ -38,6 +16,7 @@ const calculateTime = function(milliseconds) {
 // calls createTweetElement for each tweet
 // takes return value and appends it to the tweets container
 const renderTweets = function(tweets) {
+  $('#tweetcontainer').empty();
   for (const tweet of tweets) {
     $('#tweetcontainer').append(createTweetElement(tweet));
   }
@@ -66,16 +45,27 @@ $tweet.append(`
 return $tweet;
 }
 
-$(document).ready(function() {
+const loadTweets = function() {
+  $.ajax({ url: "/tweets"})
+    .then(function (res) {
+      renderTweets(res);
+  })
+}
+
+//form submission
+
   const $tweet = $("#posttweet");
   $tweet.on('submit', function (event) {
     event.preventDefault() 
     console.log('Button clicked, performing ajax call...');
     $.ajax({ url: "/tweets", method: "POST", data: $tweet.serialize()})
     .then(function () {
+      loadTweets();
       console.log("it worked????");
     });
   });
-});
 
-renderTweets(data);
+
+loadTweets();
+
+});
